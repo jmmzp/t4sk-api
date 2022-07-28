f1e90bc67408070f7a16b4f05002a2054c557d6e
José Mario <zecaomeira@gmail.com>
2022-07-28 19:33:26 -0300

    criado arquivo de rotas de tarefas

---
+++ src/routes/task.js
@@ -0,0 +1,11 @@
+const express = require('express')
+
+const routes = express()
+
+//TODO - routes.get('/', getTasks)
+//TODO - routes.get('/:id', getTaskById)
+//TODO - routes.post('/', createTask)
+//TODO - routes.put('/:id', updateTask)
+//TODO - routes.delete('/:id', deleteTask)
+
+module.exports = routes
