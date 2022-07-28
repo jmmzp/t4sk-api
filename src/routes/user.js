c0e3ddafd9ee44631d72937d431cc885d76bb3a1
José Mario <zecaomeira@gmail.com>
2022-07-28 19:33:09 -0300

    criado arquivo de rotas de usuario

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,18 +1,9 @@
 const express = require('express')
-const createProject = require('../controllers/createProject')
-const getProjectById = require('../controllers/getProjectById')
-const getProjects = require('../controllers/getProjects')
-const updateProject = require('../controllers/updateProject')
 
 const routes = express()
 
 routes.get('/verificar_token', (req, res) => {
 	res.status(200).send()
 })
-
-routes.get('/projetos', getProjects)
-routes.get('/projetos/:id', getProjectById)
-routes.post('/projetos', createProject)
-routes.put('/projetos/:id', updateProject)
 
 module.exports = routes
