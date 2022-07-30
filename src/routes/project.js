5269dbd1efff0c2b05e724fd752bd097c6f28eed
José Mario <zecaomeira@gmail.com>
2022-07-30 03:40:48 -0300

    organizando rota para lidar com invites

--- src/routes/project.js
+++ src/routes/project.js
@@ -5,7 +5,6 @@ const deleteProject = require('../controllers/project/deleteProject')
 const getPendingInvites = require('../controllers/project/getPendingInvites')
 const getProjectById = require('../controllers/project/getProjectById')
 const getProjects = require('../controllers/project/getProjects')
-const updatePendingInvites = require('../controllers/project/updatePendingInvites')
 const updateProject = require('../controllers/project/updateProject')
 
 const routes = express()
@@ -16,7 +15,6 @@ routes.get('/:id', getProjectById)
 routes.post('/', createProject)
 routes.post('/:id/adicionar_usuario', addUserToTheProject)
 routes.put('/:id', updateProject)
-routes.put('/:id/convites', updatePendingInvites)
 routes.delete('/:id', deleteProject)
 
 module.exports = routes
--- src/routes/user.js
+++ src/routes/user.js
@@ -1,9 +1,12 @@
 const express = require('express')
+const handlePendingInvite = require('../controllers/user/handlePendingInvite')
 
 const routes = express()
 
 routes.get('/verificar_token', (req, res) => {
 	res.status(200).send()
 })
+
+routes.put('/:id/convites', handlePendingInvite)
 
 module.exports = routes
